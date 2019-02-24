0382108b36e7f147d72eff97d01bb65ef041349a
Nat Grant <email.natgrant@gmail.com>
2019-02-25 12:11:37 +1300

    db function setup

--- server/db/todos.js
+++ server/db/todos.js
@@ -1,1 +1,18 @@
 const connection = require("./connection");
+
+function getAll(testDB) {
+  const db = testDB || connection;
+  return db("todos");
+}
+
+// Will need user ID eventually
+function createTodo(todo, testDB) {
+  const db = testDB || connection;
+
+  return db("todo").insert(todo);
+}
+
+module.exports = {
+  getAll,
+  createTodo
+};
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const { getAll } = require("../db/todos");
 
 //GET /api/v1/todos
 router.get("/", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      text: "hello"
-    }
-  ]);
+  getAll()
+    .then(todos => {
+      res.json(todos);
+    })
+    .catch(err => {
+      console.log(err);
+      res.setStatus(500).json({ error: "Oh no an error" });
+    });
 });
 
 // router.get("/category/:category", (req, res) => {});
