d239333fb826cd3eaa6d89b2328f6484be975c28
Nat Grant <email.natgrant@gmail.com>
2019-02-25 17:10:15 +1300

    client side set up

---
+++ client/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hi!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
