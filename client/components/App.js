a84e94ff20a6237316016e1b69082d4bc169edd9
Nat Grant <email.natgrant@gmail.com>
2019-02-25 20:22:38 +1300

    todolist displays in app

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { getTodos } from "../actions";
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.dispatch(getTodos());
+  }
+
   render() {
     return (
       <div>
-        <p>Hi!</p>
+        <ul>
+          {this.props.todos.map(todo => {
+            return <li>{todo.task}</li>;
+          })}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    todos: state.todos
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(App);
